37283386bd6f65dd516a8b3ec8650dec8652ec45
HenryRRiosLopez <hriosl@fcpn.edu.bo>
2021-04-03 21:19:59 -0400

    solucion pregunta dos

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+$ci=$_GET['ci'];
+$color=$_GET['color'];
+$nombre=$_GET['nombre'];
+echo "<h1>BIENVENIDO  ".$nombre."</h1> ";
+echo "<a href=home.php?ci=".$ci."&color=d60502&nombre=".$nombre.">";
+echo "<button>Rojo</button>";
+echo "</a>";
+echo "<a href=home.php?ci=".$ci."&color=0232d6&nombre=".$nombre.">";
+echo "<button>Azul</button>";
+echo "</a>";
+echo "<a href=home.php?ci=".$ci."&color=d6dd02&nombre=".$nombre.">";
+echo "<button>Amarillo</button>";
+echo "</a>";
+echo "<html>";
+echo "	<head>";
+echo "        <meta charset=".'utf-8'.">";
+echo "		<title>Pagina de prueba</title>";
+
+echo "<style>";
+echo "  body {background-color:".$color.";}";
+echo "</style";
+echo "</head>";
+echo "<body>";
+echo "</body";
+
+
+?>
